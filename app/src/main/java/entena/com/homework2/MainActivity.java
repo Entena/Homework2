275f3bb52ba7057bd0bc482120bfa60f0b727cca
Entena <colonialcolonel@gmail.com>
2015-02-25 22:17:23 -0500

    App now displays and saves photos after taking the picture.

--- app/src/main/java/entena/com/homework2/MainActivity.java
+++ app/src/main/java/entena/com/homework2/MainActivity.java
@@ -1,19 +1,70 @@
 package entena.com.homework2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1024;
+    private ImageView img;
+    private Button camBtn;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
+        camBtn = (Button) findViewById(R.id.btnCamera);
+        camBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // to save picture remove comment
+            File file = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+            if(file.exists()){//If the file exists delete it and overwrite
+                file.delete();
+            }
+            Log.e("CREATEINTENT", file.getPath());//Give the intent the location to save to
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+            // start camera activity
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.e("INTENTDONE", "Loading image");
+            img.setImageURI(Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "photo.jpg")));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,12 +79,11 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            super.onStop();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
