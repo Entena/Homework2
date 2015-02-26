e39f908716273839b08f44e33c010204825fc317
Entena <colonialcolonel@gmail.com>
2015-02-26 14:35:08 -0500

    App now properly runs grey conversion in background and updates progress bar.

--- app/src/main/java/entena/com/homework2/MainActivity.java
+++ app/src/main/java/entena/com/homework2/MainActivity.java
@@ -3,8 +3,10 @@ package entena.com.homework2;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
@@ -15,8 +17,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -25,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1024;
     private ImageView img;
     private Button camBtn;
-    private String mCurrentPhotoPath;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,8 @@ public class MainActivity extends ActionBarActivity {
                 dispatchTakePictureIntent();
             }
         });
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 
 
@@ -81,9 +92,159 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            super.onStop();
+            super.finish();
             return true;
         }
+        if(id == R.id.grey){
+            ImageTask task = new ImageTask();
+            try {
+                //Bitmap bmp = task.execute("grey").get();
+                Log.e("THREADING", "BEGIN ASYNC");
+                task.execute("grey");
+                //Log.e("THREADING", "GREY CONVERTED NOW SET");
+                //Log.e("THREADING", bmp.toString());
+                //img.setImageBitmap(bmp);
+            }catch (Exception e){
+                Log.e("THREADING", e.toString());
+            }
+        }
+        if(id == R.id.floyd){
+            //Call floyd
+        }
+        if(id == R.id.hokie){
+            //Call hokie filter
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private class ImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected Bitmap doInBackground(String... params) {
+            try {
+               String mode = params[0];
+               switch(mode){
+                   case "grey":
+                       Log.e("THREADING", "CALLING GREY CONVERSION");
+                       return greyScale();
+                   case "":
+
+                   default:
+                       Log.e("THREADING", "UNKNOWN MODE "+mode);
+                       return null;
+               }
+            } catch (Exception e) {
+                Log.e("THREADING", "ERROR "+e.toString());
+                return null;
+            }
+        }
+
+       protected void onProgressUpdate(int progress) {
+            super.onProgressUpdate();
+            if(progress % 10 == 0) {
+                Log.e("PROGRESS", "UPDATING " + progress);
+            }
+            progressBar.setProgress(progress);
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            progressBar.setVisibility(View.INVISIBLE);
+            if(result != null) {
+                Log.e("UPDATE UI", "UPDATE "+result.toString());
+                img.setImageBitmap(result);
+            } else {
+                Toast.makeText(getApplicationContext(), "ERROR CONVERTING IMAGE", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+        private Bitmap greyScale(){
+            File file = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+            if(file.exists()){
+                Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                myBitmap = convertToMutable(myBitmap);
+                int imgWidth = myBitmap.getWidth();
+                int imgHeight = myBitmap.getHeight();
+                for(int x =0;x<imgWidth;x++) {
+                    for(int y=0;y<imgHeight;y++) {
+                        int s = myBitmap.getPixel(x, y);
+                        int A = Color.alpha(s);
+                        int R = Color.red(s);
+                        int G = Color.green(s);
+                        int B = Color.blue(s);
+                        R = G = B = (int)((R+G+B)/3);
+                        myBitmap.setPixel(x, y, Color.argb(A, R, G, B));
+                    }
+                    //Have to use doubles or less value is always zero
+                    double i = ((double)x/(double)imgWidth)*100;
+                    int prog = (int) i;//Cast double to int then pass to update progress bar
+                    onProgressUpdate(prog);
+                }
+                Log.e("THREADING", "Returning grey image "+myBitmap.toString());
+                return myBitmap;
+            } else {
+                return null;
+            }
+        }
+
+        /**
+         * Converts a immutable bitmap to a mutable bitmap. This operation doesn't allocates
+         * more memory that there is already allocated.
+         *
+         * @param imgIn - Source image. It will be released, and should not be used more
+         * @return a copy of imgIn, but muttable.
+         */
+        public Bitmap convertToMutable(Bitmap imgIn) {
+            try {
+                //this is the file going to use temporally to save the bytes.
+                // This file will not be a image, it will store the raw image data.
+                File file = new File(Environment.getExternalStorageDirectory() + File.separator + "temp.tmp");
+
+                //Open an RandomAccessFile
+                //Make sure you have added uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"
+                //into AndroidManifest.xml file
+                RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+
+                // get the width and height of the source bitmap.
+                int width = imgIn.getWidth();
+                int height = imgIn.getHeight();
+                Bitmap.Config type = imgIn.getConfig();
+
+                //Copy the byte to the file
+                //Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
+                FileChannel channel = randomAccessFile.getChannel();
+                MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes()*height);
+                imgIn.copyPixelsToBuffer(map);
+                //recycle the source bitmap, this will be no longer used.
+                imgIn.recycle();
+                System.gc();// try to force the bytes from the imgIn to be released
+
+                //Create a new bitmap to load the bitmap again. Probably the memory will be available.
+                imgIn = Bitmap.createBitmap(width, height, type);
+                map.position(0);
+                //load it back from temporary
+                imgIn.copyPixelsFromBuffer(map);
+                //close the temporary file and channel , then delete that also
+                channel.close();
+                randomAccessFile.close();
+
+                // delete the temp file
+                file.delete();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return imgIn;
+        }
     }
 }
